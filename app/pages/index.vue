833e616fd18224e7157cdb97ca9ebe904c4b478f
Floweet <coollol6453@gmail.com>
2026-08-17 21:32:26 +0300

    feat: add base pages for future development
    - Create index.vue (main page)
    - Create profile.vue with dynamic [userId] route for user profiles
    - Establish foundation for future feature implementation

---
+++ app/pages/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="content">
+    <h1 class="content__title">Главная страница</h1>
+    <p class="content__description">Пока пусто</p>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.content {
+  display: flex;
+  flex-direction: column;
+  height: 100dvh;
+  text-align: center;
+
+  &__title {
+    margin: 0;
+  }
+
+  &__description {
+    flex: 1;
+    margin: 0;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+}
+</style>
---
+++ app/pages/user/[userId]/profile.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup>
+const userId = useRoute().params.userId as string;
+</script>
+
+<template>
+  <div>
+    <p>Профиль пользователя: {{ userId }}</p>
+  </div>
+</template>
